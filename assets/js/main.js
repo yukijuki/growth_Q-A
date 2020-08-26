f4a56d2de84706e9cb046392ea699b87e2195a24
Yuki Kawano <yukijukisfc@gmail.com>
2020-08-27 00:17:46 +0900

    js improvised

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,33 @@ var jscrollOption = {
 
 $('.jscroll').jscroll(jscrollOption);
 
+function posts_post() {
+
+    const URL = "https://growthqa.du.r.appspot.com/";
+        
+        for (let i = 1; i < latlng_list.length; i++) {
+
+            var lat = latlng_list[i][0];
+            var lon = latlng_list[i][1];
+
+            station_list.push(
+                fetch(URL + lon + "&y=" + lat)
+                .then(response => response.json())
+                .then(data => {
+                    var station= "駅,";
+                    for (var i = 0; i < data["response"]["station"].length; i++) {
+                        if (Number(data["response"]["station"][i]["distance"].slice( 0, -1 )) < 501) {
+                            station += data["response"]["station"][i]["name"]+"駅"+","
+                        }
+                    }
+                    return station
+                })
+            );
+        }
+
+        Promise.allSettled(station_list).then(values => nearstationDOM(values))
+}
+
 //post生成
 var a = document.createElement("a");
 a.className = "card-body secList";
