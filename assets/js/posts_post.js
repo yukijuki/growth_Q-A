b0074b0688edd6bcdbd3d10c23eeb1fdaa32e097
Yuki Kawano <yukijukisfc@gmail.com>
2020-08-30 00:06:05 +0900

    yuki

--- assets/js/posts_post.js
+++ assets/js/posts_post.js
@@ -1,28 +1,44 @@
+function posts_post_DOM(data) {
+    console.log(data["email"])
+}
+
 async function posts_post_api(email, title, text, category) {
-    const urls = "https://growthqa.du.r.appspot.com/posts_post?email="+ email + "&title=" + title + "&text=" + text + "&category=" + category
-    const url = "https://growthqa.du.r.appspot.com"
-    console.log(url)
+    const urls = "https://growthqa.du.r.appspot.com/posts_post?email=" + email + "&title=" + title + "&text=" + text + "&category=" + category
+    const url = "http://127.0.0.1:5000/posts_post"
+    
+    var data = {
+        "email": email,
+        "title": title,
+        "text": text,
+        "category": category
+    }
+
     return fetch(url, {
         method: 'POST',
-        headers: {"Access-Control-Allow-Origin": "*"}
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-type": "application/json"
+        },
+        dataType: 'json',
+        body: JSON.stringify(data)
       })
         .then(response => {
             if (!response.ok) {
                 return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
             } else {
-                console.log(response.json());
+                posts_post_DOM(response.json());
             }
         });
 }
 
-async function posts_post_func() {
+function posts_post_func() {
 
     const email = document.getElementById("email").value;
     const title = document.getElementById("title").value;
     const text = document.getElementById("text").value;
-    var marketer = document.getElementById("Marketer").checked;
-    var pdm = document.getElementById("PdM").checked;
-    var anlyat = document.getElementById("Data Analyst").checked;
+    const marketer = document.getElementById("Marketer").checked;
+    const pdm = document.getElementById("PdM").checked;
+    const anlyat = document.getElementById("Data Analyst").checked;
 
     var category;
 
@@ -40,5 +56,4 @@ async function posts_post_func() {
     console.log("category", category)
 
     posts_post_api(email, title, text, category)
-
 }
