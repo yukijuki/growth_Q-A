1eb209b7a64456d9b003dec08afb37a7d5e76a07
ronasuti <rnn.mpi@gmail.com>
2020-09-05 23:31:44 +0900

    added DB connection setup for app.yaml and main.py

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ import uuid
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://master@server/db"
-app.config["SECRET_KEY"] = "super-secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["JSON_AS_ASCII"] = False
 
 
-app.debug = True
+app.debug = os.environ.get('IS_DEBUG')
 db = SQLAlchemy(app)
 
 CORS(app)
